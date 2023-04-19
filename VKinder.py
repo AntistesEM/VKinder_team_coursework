ae74666504f5570036066373915e05ea4ef60ce0
Jamesmobile16 <119048241+Jamesmobile16@users.noreply.github.com>
2023-04-19 22:06:12 +0300

    Update VKinder.py

--- VKinder.py
+++ VKinder.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from datetime import datetime
 from pprint import pprint
 import re
 
@@ -21,22 +22,18 @@ class VKinder:
         fields = {'fields': 'bdate, city, sex, music, books'}
         response = requests.get(url, params={**self.params, **params, **fields}).json()
         self_info = {}
-        year = self.get_birthday_year(response['response'][0]['bdate'])
-        self_info['year'] = year
-        self_info['city'] = response['response'][0]['city']['title']
+        bdate = response['response'][0]['bdate']
+        bdate = bdate.split(sep='.')
+        self_year = int(bdate[2])
+        current_year = datetime.now().date().year
+        self_age = current_year - self_year
+        self_info['age'] = self_age
+        self_info['city'] = response['response'][0]['city']['id']
         self_info['sex'] = response['response'][0]['sex']
         self_info['books'] = response['response'][0]['books']
         self_info['music'] = response['response'][0]['music']
         self_info['groups'] = self.groups_get(self.id)['response']['items']
         return self_info
-
-    def get_birthday_year(self, bdate: str): #Функция обрабатывает полученную дату рождения пользователя и возвращает только год.
-        reg = re.match(r'([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{4})', bdate)
-        if reg != None:
-            year = reg.group(3)
-        else:
-            year = 0
-        return year
 
     def groups_get(self, user_id): #Функция получает список групп пользователя и возвращает словарь.
         url = 'https://api.vk.com/method/groups.get'
@@ -49,88 +46,84 @@ class VKinder:
 
     def users_search(self):
         #Функция запрашивает пользователей VK (х1000), далее фильтрует их, сравнивая с текущим пользователем.
-        #В category1_search попадают пользователи, подожедшие по базовым критериям (город, пол, возварст)
-        #В category2_search попадают пользователи после первой фильтрации, у которых есть одинаковые группы.
-        #В category3_search попадают пользователи после второй фильтрации, у которых есть совпадения либо по
-        #общим книгам, либо по музыке. Функция возвращает список пользователей со всеми данными, либо после 3ей фильтрации,
-        # либо, если таких не найдено, после предыдущей.
-        url = 'https://api.vk.com/method/users.search'
-        fields = {'count': 1000, 'fields': 'bdate, city, sex, music, books'}
-        response = requests.get(url, params={**self.params, **fields}).json()
+        #В category1_search попадают пользователи, подожедшие по критериям (город, пол, возварст) и общим группам.
+        #В category2_search попадают пользователи после первой фильтрации, у которых есть совпадения либо по
+        #общим книгам, либо по музыке. Функция возвращает список пользователей со всеми данными, либо после 2ой фильтрации,
+        # либо, если таких не найдено, после базовой.
         self_info = self.self_info()
-        self_year = self_info['year']
+        self_age = self_info['age']
         self_city = self_info['city']
         self_sex = self_info['sex']
-        self_groups = self_info['groups']
         self_music = self_info['music']
         self_music = ''.join(self_music.split()).lower().split(',')
         self_books = self_info['books']
         self_books = ''.join(self_books.split()).lower().split(',')
+        if self_sex == 1:
+            search_user_sex = 2
+        else:
+            search_user_sex = 1
+        search_user_age_from = self_age - 5
+        search_user_age_to = self_age + 5
+        url = 'https://api.vk.com/method/users.search'
+        fields = {
+            'count': 1000,
+            'sex': search_user_sex,
+            'city_id': self_city,
+            'has_photo': 1,
+            'age_from': search_user_age_from,
+            'age_to': search_user_age_to,
+            'fields': 'bdate, city, sex, music, books',
+            'group_id': self.groups_get(self.id)['response']['items']
+        }
+        response = requests.get(url, params={**self.params, **fields}).json()
         category1_search = {}
         category2_search = {}
-        category3_search = {}
         for user in response['response']['items']:
-            if 'bdate' not in user.keys():
-                pass
-            elif 'city' not in user.keys():
-                pass
-            else:
-                user_year = self.get_birthday_year(user['bdate'])
-                if user_year == self_year\
-                    and user['city']['title'] == self_city\
-                    and user['sex'] != self_sex:
-                    category1_search[user['id']] = user
+            category1_search[user['id']] = user
         for user1 in category1_search.values():
-            user1_groups = self.groups_get(user1['id'])
-            time.sleep(0.3)
-            if user1_groups != None:
-                for group in self_groups:
-                    if group in user1_groups['response']['items']:
-                        category2_search[user1['id']] = user1
-        for user2 in category2_search.values():
-            if 'music' in user2.keys():
-                user2_music = user2['music']
-                user2_music = ''.join(user2_music.split()).lower().split(',')
-                for pattern in user2_music:
+            if 'music' in user1.keys():
+                user1_music = user1['music']
+                user1_music = ''.join(user1_music.split()).lower().split(',')
+                for pattern in user1_music:
                     for music in self_music:
                         match = re.search(pattern, music)
                         if match == None:
                             pass
-                        elif match.group(0) in user2_music:
-                            category3_search[user2['id']] = user2
+                        elif match.group(0) in user1_music:
+                            category2_search[user1['id']] = user1
                         else:
                             pass
             else:
                 pass
-        for user3 in category2_search.values():
-            if 'books' in user3.keys():
-                user3_books = user3['books']
-                user3_books = ''.join(user3_books.split()).lower().split(',')
-                for pattern1 in user3_books:
+        for user2 in category1_search.values():
+            if 'books' in user2.keys():
+                user2_books = user2['books']
+                user2_books = ''.join(user2_books.split()).lower().split(',')
+                for pattern1 in user2_books:
                     for book in self_books:
                         match1 = re.search(pattern1, book)
                         if match1 == None:
                             pass
-                        elif match1.group(0) in user3_books:
-                            category3_search[user3['id']] = user3
+                        elif match1.group(0) in user2_books:
+                            category2_search[user2['id']] = user2
                         else:
                             pass
             else:
                 pass
-        if len(category3_search) != 0:
-            return category3_search
-        elif len(category2_search) != 0:
+        if len(category2_search) != 0:
             return category2_search
         elif len(category1_search) != 0:
             return category1_search
 
-    def get_photos(self, self_id): #Функция возвращает фото с профиля пользователя в максимальном разрешении.
+    def get_photos(self, self_id):
+        # Функция возвращает 3 фото с профиля пользователя в максимальном разрешении,
+        # с наибольшим кол-вом лайков
         url = 'https://api.vk.com/method/photos.get'
         params = {'owner_id': self_id,
                   'album_id': 'profile',
                   'extended': '1',
                   'photo_sizes': '0',
-                  'count': 3}
+                  'count': 1000}
         response = requests.get(url, params={**self.params, **params}).json()
         photo_list = response['response']['items']
         photo_to_upload = []
@@ -144,11 +137,15 @@ class VKinder:
                     pass
                 b.update(photo_info['likes'])
                 b['date'] = photo_info['date']
-            photo_to_upload.append(b)
+            a = (b['count'], b['url'])
+            photo_to_upload.append(a)
+        photo_to_upload.sort(reverse=1)
+        n = 3
+        photos_list = [x for index, x in enumerate(photo_to_upload) if index < n]
+        return photos_list
 
-        return photo_to_upload
-
-    def get_users_info(self): #Функция возвращает нужные данные найденных ранее пользоватедей (Имя, ссылку, фото х3)
+    def get_users_info(self):
+        #Функция возвращает нужные данные найденных ранее пользоватедей (Имя, ссылку, фото х3)
         users_info = []
         ids = self.users_search()
         for user_id in ids.values():
@@ -156,7 +153,7 @@ class VKinder:
             photo_list = self.get_photos(user_id['id'])
             photo_url_list = []
             for photo in photo_list:
-                photo_url_list.append(photo['url'])
+                photo_url_list.append(photo[1])
             user_info['photos'] = photo_url_list
             user_info['first_name'] = user_id['first_name']
             user_info['last_name'] = user_id['last_name']
@@ -165,14 +162,13 @@ class VKinder:
         return users_info
 
 
-if __name__ =='__main__':
+if __name__=='__main__':
 
     vkinder = VKinder(token, vk_id)
-    vkinder.get_users_info()
-
+    # vkinder.get_users_info()
     # pprint(vkinder.self_info())
     # pprint(vkinder.users_search())
     # print(vkinder.groups_get())
     # pprint(vkinder.get_photos())
-    # pprint(vkinder.get_users_info())
+    pprint(vkinder.get_users_info())
 
